b5e6a19c25d879fc99ab33701cd47a20cc73766f
leskal <alexakis1997@gmail.com>
2018-01-18 01:40:36 +0200

    Scripts for building kernel and make config
    Change-Id: I212bfcfa8aaa1c7c9e2fd3059ef939f31640e9bb

---
+++ makekernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DEFCONFIG=lineage_oxygen_defconfig
+KERNEL=lineage
+if [  -f $KERNEL ]; then
+	rm $KERNEL
+fi
+if [  -f $DEFCONFIG ]; then
+	rm $DEFCONFIG
+fi
+make clean && make mrproper
+export CROSS_COMPILE=~/android/android-ndk-r14b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+export ARCH=arm64
+make -j8 $DEFCONFIG
+make -j8
+cp arch/arm64/boot/Image.gz-dtb $KERNEL
